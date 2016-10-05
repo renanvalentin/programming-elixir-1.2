d69ac8490f2866c7d14f6a539b476b4bceb78c0b
Renan Ferreira <renan.lifestyle@hotmail.com>
2016-10-05 06:47:48 -0300

    struct

---
+++ ch_8_data_struct/query.exs
@@ -0,0 +1,10 @@
+people = [
+  %{ name: "Grumpy", height: 1.24},
+  %{ name: "Dave", height: 1.88},
+  %{ name: "Dopey", height: 1.32},
+  %{ name: "Shaquille", height: 2.16}
+]
+
+IO.inspect(for person = %{ height: height } <- people,
+                        height > 1.5,
+           do: person)

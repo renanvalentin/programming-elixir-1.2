a0a5f10e062ce0d5ca5437f8b58c502f8a509d02
Renan Ferreira <renan.lifestyle@hotmail.com>
2017-08-22 22:35:10 -0300

    create project documentation

--- issues/mix.exs
+++ issues/mix.exs
@@ -5,6 +5,8 @@ defmodule Issues.Mixfile do
     [app: :issues,
      escript: escript_config,
      version: "0.1.0",
+     name: "Issues",
+     source_url: "https://github.com/renanvalentin/programming-elixir-1.2/issues",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -30,7 +32,9 @@ defmodule Issues.Mixfile do
   defp deps do
     [
       { :httpoison, "~> 0.11.1" },
-      {  :poison, "~> 3.0" }
+      { :poison, "~> 3.0" },
+      { :ex_doc, "~> 0.11" },
+      { :earmark, ">= 0.0.0" }
     ]
   end
 

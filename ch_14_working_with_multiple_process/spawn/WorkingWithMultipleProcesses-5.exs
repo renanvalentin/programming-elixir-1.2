4edfb6110b7c18d413c9df003fb286a739561192
Renan Ferreira <renan.lifestyle@hotmail.com>
2017-08-24 23:09:35 -0300

    monitoring a process

---
+++ ch_14_working_with_multiple_process/spawn/WorkingWithMultipleProcesses-5.exs
@@ -0,0 +1,28 @@
+# inspired by Patrick Oscity's solution
+# Stefan Chrobot has another clever idea iex> h exit
+defmodule Ex3 do
+  import :timer, only: [ sleep: 1 ]
+
+  def messenger(recipient) do
+    send recipient, { :ok, "Ao" }
+    raise RuntimeError, message: "Holy Cow!"
+  end 
+
+  def receive_messages do
+    receive do
+      message ->
+        IO.puts "result: #{inspect message}"
+        receive_messages
+    after 500 ->
+      IO.puts "ending..."
+    end
+  end
+
+  def run do
+    spawn_monitor(Ex3, :messenger, [self])
+    sleep 500
+    receive_messages
+  end
+end
+
+Ex3.run
